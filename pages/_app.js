715ab3c8a1af414c4ca983381a06c7971c638858
mohammad <ghanimmohmmad@gmail.com>
2025-06-24 18:39:43 +0300

    fix 4

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,12 @@ import '../styles/globals.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { CartProvider } from '../context/CartContext';
+import { getSession } from 'next-auth/react'; // <--- استيراد getSession
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-  console.log("MyApp session:", session);
+function MyApp({ Component, pageProps }) { // <--- لا تقم بـ destructure هنا، مرر pageProps كاملة
+  console.log("MyApp session:", pageProps.session); // للتأكد من أنها لم تعد undefined
   return (
-    <SessionProvider session={session}>
+    <SessionProvider session={pageProps.session}> {/* <--- تمرير pageProps.session */}
      <CartProvider>
       <Layout>
         <Component {...pageProps} />
@@ -17,5 +18,15 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
     </SessionProvider>
   );
 }
+
+// <--- إضافة getInitialProps لجلب الجلسة على جانب الخادم
+MyApp.getInitialProps = async (context) => {
+  const session = await getSession(context); // جلب الجلسة
+  return {
+    pageProps: {
+      session, // تمرير الجلسة التي تم جلبها إلى pageProps
+    },
+  };
+};
 
 export default MyApp;
